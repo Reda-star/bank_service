b9c086a37fbbcb9c6832f8b888ffe04aab7741c8
Reda Belouch <80953267+Reda-star@users.noreply.github.com>
2022-10-07 15:06:52 +0100

    Add files via upload

---
+++ src/main/java/org/sid/ebankservice/entities/BankAccount.java
@@ -0,0 +1,23 @@
+package org.sid.ebankservice.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.ebankservice.enums.AccountType;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class BankAccount {
+    @Id
+    private String id;
+    private Date createdAt;
+    private Double balance;
+    private AccountType type;
+    private String currency;
+}
